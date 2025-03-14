6210d1ebe16a8fc25c98d0e2c101c54e83641052
Demondra <rahajarisonraphael@gmail.com>
2025-03-13 16:23:23 -0800

    resolution du bug de compilation

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_macos
+import connectivity_plus
 import file_selector_macos
 import flutter_image_compress_macos
 import path_provider_foundation
